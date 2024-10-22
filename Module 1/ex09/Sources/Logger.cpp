0a2cf775eaf3ff33e2f2b6bfa5156f14de172286
AMarieTnS <a.marie@technologyandstrategy.com>
2024-10-22 13:55:25 +0200

    Update Logger.cpp
    If already a log, add another one below

--- Module 1/ex09/Sources/Logger.cpp
+++ Module 1/ex09/Sources/Logger.cpp
@@ -7,33 +7,33 @@
 #include <string>
 
 void Logger::logToConsole(std::string const &sLogMessageConsole) {
-  std::cout << sLogMessageConsole << std::endl;
+  std::cout << sLogMessageConsole << std::endl << std::endl;
 }
 
 void Logger::logToFile(std::string const &sLogMessageFile) {
   std::fstream MyFile("LogFile.txt",
-                      std::ios::in | std::ios::out | std::ios::trunc);
+                      std::ios::in | std::ios::out | std::ios::app);
   if (!MyFile) {
     std::cerr << "Error: Could not open file " << "LogFile.txt" << std::endl;
     return;
   }
-  MyFile << sLogMessageFile;
+  MyFile << sLogMessageFile << std::endl;
   MyFile.seekg(0, std::ios::beg);
 
   std::string sContent((std::istreambuf_iterator<char>(MyFile)),
                        std::istreambuf_iterator<char>());
-  std::cout << "Content after writing logs in file: " << sContent << std::endl;
-  std::cout << sLogMessageFile << std::endl;
+  std::cout << "Content after writing logs in file: " << std::endl
+            << sContent << std::endl;
 }
 
 std::string Logger::makeLogEntry(std::string const &sLogMessageEntry) {
   auto now = std::chrono::system_clock::now();
-        auto in_time_t = std::chrono::system_clock::to_time_t(now);
-        std::tm buf;
-        localtime_s(&buf, &in_time_t);
-        std::ostringstream oss;
-        oss << std::put_time(&buf, "%Y-%m-%d %H:%M:%S");
-        return oss.str() + ": " + sLogMessageEntry;
+  auto in_time_t = std::chrono::system_clock::to_time_t(now);
+  std::tm buf;
+  localtime_s(&buf, &in_time_t);
+  std::ostringstream oss;
+  oss << std::put_time(&buf, "%Y-%m-%d %H:%M:%S");
+  return oss.str() + ": " + sLogMessageEntry;
 }
 
 void Logger::log(std::string const &dest, std::string const &message) {
@@ -47,5 +47,5 @@ void Logger::log(std::string const &dest, std::string const &message) {
       return;
     }
   }
-  std::cerr << "Unknown action: " << message << std::endl;
+  std::cerr << "Unknown action: " << message << std::endl << std::endl;
 }
