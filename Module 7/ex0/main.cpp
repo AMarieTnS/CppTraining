9d7a6d7786ba63c2052e39b4aa93cb3f996ed1dd
AMarieTnS <a.marie@technologyandstrateny.com>
2024-11-18 13:46:45 +0100

    Module 7 ex0 fix

--- Module 7/ex0/main.cpp
+++ Module 7/ex0/main.cpp
@@ -1,18 +1,18 @@
 #include "Whatever"
 #include <iostream>
 
-int main() {
-  int x = 10, y = 20;
-  std::cout << "Before swap: x = " << x << ", y = " << y << std::endl;
-  swap(x, y);
-  std::cout << "After swap: x = " << x << ", y = " << y << std::endl;
-
-  std::cout << "min(x, y) = " << min(x, y) << std::endl;
-  std::cout << "max(x, y) = " << max(x, y) << std::endl;
-
-  double a = 5.5, b = 2.2;
-  std::cout << "min(a, b) = " << min(a, b) << std::endl;
-  std::cout << "max(a, b) = " << max(a, b) << std::endl;
-
+int main(void) {
+  int a = 2;
+  int b = 3;
+  ::swap(a, b);
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+  std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+  ::swap(c, d);
+  std::cout << "c = " << c << ", d = " << d << std::endl;
+  std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+  std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
   return 0;
 }
